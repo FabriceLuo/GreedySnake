5a161df749b2993abe809b1584de032a4e2439d7
luo_minghao <luo_minghao@163.com>
2016-04-26 22:54:23 +0800

    add init files

---
+++ greedysnake.h
@@ -0,0 +1,15 @@
+#ifndef GREEDYSNAKE_H
+#define GREEDYSNAKE_H
+
+#include <QMainWindow>
+
+class GreedySnake : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    GreedySnake(QWidget *parent = 0);
+    ~GreedySnake();
+};
+
+#endif // GREEDYSNAKE_H
