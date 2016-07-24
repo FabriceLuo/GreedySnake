d60b00367a01ad487bc48dde28bb1e2fcc9271b4
luo_minghao <luo_minghao@163.com>
2016-07-24 17:11:10 +0800

    add modify

--- greedysnake.cpp
+++ greedysnake.cpp
@@ -2,18 +2,63 @@
 
 #include <QDesktopWidget>
 #include <QApplication>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
+#include <QMessageBox>
+#include <QPainter>
+#include <QPen>
 
 GreedySnake::GreedySnake(QWidget *parent)
-    : QMainWindow(parent), m_windowName("GreedySnake"), m_windowSize(800, 600)
+    : QMainWindow(parent), m_windowName("GreedySnake"), m_windowSize(800, 600), m_edgeLength(20)
 {
 
     setWindowTitle(m_windowName);
-    setFixedSize(m_windowSize);
+    //setFixedSize(m_windowSize);
+    resize(m_windowSize);
     QDesktopWidget *desktop = QApplication::desktop();
     move((desktop->width() - this->width())/2, (desktop->height() - this->height())/2);
+
+    initMenuBar();
 }
 
 GreedySnake::~GreedySnake()
 {
 
 }
+
+void GreedySnake::initMenuBar()
+{
+    QMenuBar *menu=menuBar();
+
+    QMenu *controlMenu = new QMenu("Control");
+    QAction *startAction= new QAction("Start", this);
+    connect(startAction, SIGNAL(triggered(bool)),this, SLOT(startGame()));
+    controlMenu->addAction(startAction);
+
+    menu->addMenu(controlMenu);
+}
+
+void GreedySnake::startGame()
+{
+
+}
+
+void GreedySnake::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setPen(QPen(Qt::black, 12, Qt::DashLine, Qt::RoundCap));
+    painter.drawLine(QPoint(20,20), QPoint(200, 200));
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- greedysnake.h
+++ greedysnake.h
@@ -2,6 +2,12 @@
 #define GREEDYSNAKE_H
 
 #include <QMainWindow>
+#include <vector>
+
+using namespace std;
+
+class QPoint;
+
 
 class GreedySnake : public QMainWindow
 {
@@ -11,10 +17,18 @@ public:
     GreedySnake(QWidget *parent = 0);
     ~GreedySnake();
 protected:
+    vector<QPoint> *m_snake;
+    //debug info
+    void printDebugLine();
+    void paintEvent(QPaintEvent *event);
 
 private:
-    QString m_windowName;
-    QSize m_windowSize;
+    QString     m_windowName;
+    QSize       m_windowSize;
+    int         m_edgeLength;
+    void        initMenuBar();
+private slots:
+    void startGame();
 
 };
 
